59a65ca5a5b7a2437e026f8da00d20746c8f4775
Raphael Adamski <raphaeladamski@hotmail.com>
2016-12-19 16:48:22 -0200

    Criando circulo

---
+++ src/Game/Circulo.java
@@ -0,0 +1,58 @@
+package Game;
+
+import Engine.components.GameComponent;
+import Engine.core.*;
+import Engine.rendering.RenderingEngine;
+import Engine.rendering.Shader;
+import static org.lwjgl.opengl.GL11.*;
+
+
+public class Circulo extends GameComponent{
+	
+	boolean fill=true;
+	float radius=0;
+	Circulo(float r){
+		this.radius=r;
+	}
+	
+	public void Render(Shader shader, RenderingEngine renderingEngine) {
+		glColor3f(1.0f, 0.0f, 0.0f);
+        double theta = (2.0f * Math.PI) / 360.0f; 
+  		double c = Math.cos(theta);//precalculate the sine and cosine
+  		double s = Math.sin(theta);
+  		double t;
+
+  		double xx = radius;//we start at angle = 0 
+  		double yy = 0; 
+  	    
+  		if (fill==true)
+  		{
+  			glBegin(GL_TRIANGLE_FAN);
+  		}
+  		else
+  		{
+  			glBegin(GL_LINE_LOOP);
+  		}
+
+  		for(int i = 0; i < 360; i++) 
+  		{ 
+  			glVertex2d(xx + GetTransform().GetPos().GetX(), yy + GetTransform().GetPos().GetY());//output vertex 
+  	        
+
+  			t = xx;
+  			xx = c * xx - s * yy;
+  			yy = s * t + c * yy;
+  		} 
+  		glEnd();
+	}
+
+
+	public boolean Inside(Vector3f point) {
+		if (Math.pow(point.GetX()-GetTransform().GetPos().GetX(),2) + 
+				Math.pow(point.GetY()-GetTransform().GetPos().GetY(),2) 
+				< Math.pow(radius,2))
+			return true;
+		return false;
+	}
+	
+}
--- src/Game/TestGame.java
+++ src/Game/TestGame.java
@@ -31,9 +31,11 @@ public class TestGame extends Game
 				(float) Window.GetWidth() / (float) Window.GetHeight(), 0.01f, 1000.0f)));
 		
 		GameObject player = new GameObject();
+		Circulo circuloComponrnt = new Circulo(150.0f);
+		player.GetTransform().SetPos(new Vector3f(200.0f,200.0f,0.0f));
 		Sprite playerSprite = new Sprite(new Texture("circulo.png"));
 		playerSprite.setScale(new Vector3f(0.5f, 0.5f, 1.0f));
-		player.AddComponent(playerSprite);
+		player.AddComponent(circuloComponrnt);
 
 		AddObject(camera);
 		AddObject(player);
